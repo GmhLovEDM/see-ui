4165c720adffcaed78e92b79e1f5704c56c7bed4
GmhLovEDM <GmhLovEDM@gmail.com>
2023-12-29 10:51:42 +0800

    refactor: (SeeButton) :art: Convertir un code vue2 en vue3

--- components/seeButton/index.vue
+++ components/seeButton/index.vue
@@ -1,152 +1,152 @@
 <template>
 	<view>
-		<button class="c-button" :class="isColor ? 'yesColor' : 'noColor'" @touchstart="touch" :style="{
-			 	width: width,
-			 	height: height,
-				borderRadius: radius,
-			 	backgroundColor: color}">
+		<button class="c-button" :class="isColor ? 'yesColor' : 'noColor'" @touchstart="touch" :style="buttonStyle">
 			<template v-if="isLoading">
-				<view style="display: flex;align-items: center;justify-content: center;">
-					<image style="width: 25rpx;height: 25rpx;"
-						src="@/static/user-loading.gif"
-						mode=""></image>
+				<view style="display: flex; align-items: center; justify-content: center;">
+					<image style="width: 25rpx; height: 25rpx;" src="@/static/user-loading.gif" mode=""></image>
 				</view>
 			</template>
 			<template v-else>
 				<slot></slot>
 			</template>
-			<view v-if="isRipple" class="waveRipple" :class="[isWaving?'waveActive':'']" :style="{
-					top: rippleToTop + 'px',
-					left: rippleToLeft + 'px',
-					width: domInfo.targetWidth + 'px',
-					height: domInfo.targetWidth + 'px',
-					backgroundColor: 'rgba(0, 0, 0, 0.15)'}">
+			<view v-if="isRipple" class="waveRipple" :class="[isWaving ? 'waveActive' : '']" :style="rippleStyle">
 			</view>
 		</button>
 	</view>
 </template>
 
-<script>
-	export default {
-		name: "c-button",
-		props: {
-			width: {
-				type: String,
-				default: uni.getSystemInfoSync().screenWidth * 0.9 + 'px'
-			},
-			height: {
-				type: String,
-				default: '94rpx'
-			},
-			color: {
-				type: String,
-				default: '#4DC3FF'
-			},
-			isColor: {
-				type: Boolean,
-				default: true
-			},
-			isRipple: {
-				type: Boolean,
-				default: true
-			},
-			radius: {
-				type: String,
-				default: '22px'
-			},
-			isLoading: {
-				type: Boolean,
-				default: false
-			}
-		},
-		data() {
-			return {
-				rippleToTop: 0,
-				rippleToLeft: 0,
-				domInfo: {},
-				isWaving: false,
-				timer: {},
-				flag: false,
-			};
-		},
-		created() {
+<script setup lang="ts">
+	import { ref, computed, watch, onMounted, nextTick, getCurrentInstance } from 'vue';
+	const instance = getCurrentInstance();
 
+	// 定义props
+	const props = defineProps({
+		width: {
+			type: String,
+			default: uni.getSystemInfoSync().screenWidth * 0.9 + 'px'
 		},
-		methods: {
-			touch(e) {
-				if (!this.isColor) return;
-				if (this.isLoading) return;
-				this.$emit('change')
-				this.throttle(() => {
-					this.isWaving = false
-					this.$nextTick(function() {
-						this.getWaveQuery(e)
-					})
-				})
-			},
-			throttle(fc, waitTime = 500, imme = true) {
-				if (imme) {
-					if (!this.flag) {
-						this.flag = true
-						typeof fc === 'function' && fc()
-						this.timer = setTimeout(() => {
-							this.flag = false
-						}, waitTime)
-					}
-				} else {
-					if (!this.flag) {
-						this.flag = true
-						this.timer = setTimeout(() => {
-							this.flag = false
-							typeof fc === 'function' && fc()
-						}, waitTime)
-					}
-				}
-			},
-			getWaveQuery(e) {
-				this.getElQuery().then(res => {
-					let data = res[0]
-					if (!data.width || !data.width) return
-					data.targetWidth = data.height > data.width ? data.height : data.width
-					if (!data.targetWidth) return
-					this.domInfo = data;
-					let touchesX = ''
-					let touchesY = ''
-					// #ifndef MP-BAIDU || MP-ALIPAY
-					touchesX = e.touches[0].clientX
-					touchesY = e.touches[0].clientY
-					// #endif
-					// #ifdef MP-BAIDU
-					touchesX = e.changedTouches[0].clientX
-					touchesY = e.changedTouches[0].clientY
-					// #endif
-					// #ifdef MP-ALIPAY
-					touchesX = e.detail.clientX
-					touchesY = e.detail.clientY
-					// #endif
-					this.rippleToTop = touchesY - data.top - data.targetWidth / 2
-					this.rippleToLeft = touchesX - data.left - data.targetWidth / 2
-					this.$nextTick(() => {
-						this.isWaving = true
-					})
-				})
-			},
-			getElQuery() {
-				return new Promise(resolve => {
-					let queryInfo = ''
-					queryInfo = uni.createSelectorQuery().in(this)
-					//#ifdef MP-ALIPAY
-					queryInfo = uni.createSelectorQuery()
-					//#endif
-					queryInfo.select('.c-button').boundingClientRect()
-					queryInfo.exec(data => {
-						resolve(data)
-					})
-				})
-			},
+		height: {
+			type: String,
+			default: '94rpx'
+		},
+		color: {
+			type: String,
+			default: '#4DC3FF'
+		},
+		isColor: {
+			type: Boolean,
+			default: true
+		},
+		isRipple: {
+			type: Boolean,
+			default: true
+		},
+		radius: {
+			type: String,
+			default: '22px'
+		},
+		isLoading: {
+			type: Boolean,
+			default: false
 		}
-	}
+	});
+
+	// 定义data
+	const rippleToTop = ref(0);
+	const rippleToLeft = ref(0);
+	const domInfo = ref<{ targetWidth ?: string }>({});
+	const isWaving = ref(false);
+	let flag = ref(false);
+
+	// 按钮样式
+	const buttonStyle = computed(() => ({
+		width: props.width,
+		height: props.height,
+		borderRadius: props.radius,
+		backgroundColor: props.color
+	}));
+
+	// 水波样式
+	const rippleStyle = computed(() => ({
+		top: rippleToTop.value + 'px',
+		left: rippleToLeft.value + 'px',
+		width: domInfo.value.targetWidth + 'px',
+		height: domInfo.value.targetWidth + 'px',
+		backgroundColor: 'rgba(0, 0, 0, 0.15)'
+	}));
+
+	const throttle = (fc, waitTime = 500, imme = true) => {
+		if (imme) {
+			if (!flag.value) {
+				flag.value = true;
+				typeof fc === 'function' && fc();
+				setTimeout(() => {
+					flag.value = false;
+				}, waitTime);
+			}
+		} else {
+			if (!flag.value) {
+				flag.value = true;
+				setTimeout(() => {
+					flag.value = false;
+					typeof fc === 'function' && fc();
+				}, waitTime);
+			}
+		}
+	};
+
+	const touch = (e) => {
+		if (!props.isColor || props.isLoading) return;
+		// emit('change');
+		throttle(() => {
+			isWaving.value = false;
+			nextTick(() => {
+				getWaveQuery(e);
+			});
+		});
+	};
+
+	const getWaveQuery = async (e) => {
+		const res = await getElQuery();
+		let data = res[0];
+		if (!data.width || !data.height) return;
+		data.targetWidth = data.height > data.width ? data.height : data.width;
+		if (!data.targetWidth) return;
+		domInfo.value = data;
+		// 根据不同平台获取touches的位置
+		let touchesX = e.touches[0].clientX || null;
+		let touchesY = e.touches[0].clientY || null;
+		rippleToTop.value = touchesY - data.top - data.targetWidth / 2;
+		rippleToLeft.value = touchesX - data.left - data.targetWidth / 2;
+		nextTick(() => {
+			isWaving.value = true;
+		});
+	};
+
+	const getElQuery = () => {
+		return new Promise(resolve => {
+			let queryInfo = uni.createSelectorQuery().in(instance.proxy);
+			//#ifdef MP-ALIPAY
+			queryInfo = uni.createSelectorQuery();
+			//#endif
+			queryInfo.select('.c-button').boundingClientRect();
+			queryInfo.exec(data => {
+				resolve(data);
+			});
+		});
+	};
+
+	// 生命周期钩子
+	onMounted(() => {
+		// 在组件挂载后执行的代码
+	});
+
+	// 监听
+	watch(() => props.isLoading, (newVal, oldVal) => {
+		// isLoading发生变化时执行的代码
+	});
 </script>
+
 
 <style lang="scss" scoped>
 	.c-button {
